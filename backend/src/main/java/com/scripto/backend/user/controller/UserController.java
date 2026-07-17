f26f021ddf086c353d0fe24a031d043b5f5a4dc3
José Júnior <59178745+josec-junior@users.noreply.github.com>
2026-07-17 12:56:28 -0300

    feat: implementa login de usuários com token JWT

--- backend/src/main/java/com/scripto/backend/user/controller/UserController.java
+++ backend/src/main/java/com/scripto/backend/user/controller/UserController.java
@@ -1,18 +1,26 @@
 package com.scripto.backend.user.controller;
 
+import com.scripto.backend.auth.dto.LoginDTO;
+import com.scripto.backend.auth.dto.TokenJWTDTO;
 import com.scripto.backend.auth.dto.UserRegisterDTO;
 import com.scripto.backend.user.dto.UserUpdateDTO;
 import com.scripto.backend.user.service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.token.TokenService;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user/")
 public class UserController {
+
     private final UserService userService;
+
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -45,6 +53,12 @@ public class UserController {
         }
         this.userService.registerUser(userRegisterDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/login/")
+    public ResponseEntity<TokenJWTDTO> loginUser(@RequestBody @Valid LoginDTO loginDTO) {
+        var token = this.userService.loginUser(loginDTO);
+        return ResponseEntity.ok(new TokenJWTDTO(token));
     }
 
 }
--- backend/src/main/java/com/scripto/backend/user/service/UserService.java
+++ backend/src/main/java/com/scripto/backend/user/service/UserService.java
@@ -1,12 +1,18 @@
 package com.scripto.backend.user.service;
 
+import com.scripto.backend.auth.dto.LoginDTO;
 import com.scripto.backend.auth.dto.UserRegisterDTO;
+import com.scripto.backend.security.JwtService;
 import com.scripto.backend.user.dto.UserUpdateDTO;
 import com.scripto.backend.user.dto.UserViewDTO;
 import com.scripto.backend.user.entity.User;
 import com.scripto.backend.user.repository.UserRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.token.TokenService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,9 +24,14 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JwtService jwtService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
     }
 
 
@@ -56,5 +67,11 @@ public class UserService {
         String encryptedPassword = passwordEncoder.encode(userRegisterDTO.password());
         User user = new User(userRegisterDTO.fullName(), userRegisterDTO.email(), userRegisterDTO.cpf(), encryptedPassword);
         this.userRepository.save(user);
+    }
+
+    public String loginUser(@Valid LoginDTO loginDTO) {
+        var authenticationToken = new UsernamePasswordAuthenticationToken(loginDTO.email(), loginDTO.password());
+        var authentication = authenticationManager.authenticate(authenticationToken);
+        return jwtService.generateToken((User) authentication.getPrincipal());
     }
 }
